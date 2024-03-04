d61ee5ce57e65ad4699403e3c0495f05abe54519
Alberto CLl <160025496+Terrimega@users.noreply.github.com>
2024-03-04 13:39:56 +0100

    Add files via upload

---
+++ launcher.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "$$"
+
+# Change directory to where the Python script is located
+cd /mnt/c/Users/alber/PycharmProjects/pythonProject/Practice/Tests
+
+# Run the Python script
+python3 Fterminal.py
+
